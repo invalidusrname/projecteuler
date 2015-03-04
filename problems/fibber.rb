88acf906930e0fa22a24add6ea047d8ca2454fa5
Matt McMahand <mmcmahand@gmail.com>
2015-03-03 20:49:59 -0500

    implementation!

--- problems/fibber.rb
+++ problems/fibber.rb
@@ -1,2 +1,28 @@
 class Fibber
+  def self.generate(target)
+    fibs = []
+    fib = 1
+    next_fib = 1
+
+    while next_fib <= target do
+      fibs << next_fib
+      next_fib = fib + next_fib
+      fib = fibs.last
+    end
+
+    fibs
+  end
+
+  def self.sum_evens(target)
+    targets = generate(target)
+    total = targets.inject(0) do |sum, x|
+      if x % 2 == 0 
+        sum + x
+      else
+        sum 
+      end
+    end
+
+    total
+  end
 end
--- spec/fibber_spec.rb
+++ spec/fibber_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe Fibber do
-  it "generates the first 10 fibonacci numbers"
-  it "knows the sum of even numbers less than 100"
+  it "generates fib numbers up to the provided number" do
+    fibs_up_to_10 = [1, 2, 3, 5, 8]
+    expect(Fibber.generate(10)).to eq(fibs_up_to_10)
+
+    fibs_up_to_100 = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    expect(Fibber.generate(100)).to eq(fibs_up_to_100)
+  end
+
+  it "knows the sum of even numbers less than 100" do
+    expect(Fibber.sum_evens(100)).to eq(44)
+    expect(Fibber.sum_evens(10)).to eq(10)
+  end
+
+  it "knows how to produce the sum of even fibs up to 4 million" do
+    expect(Fibber.sum_evens(4_000_000)).to eq(4613732)
+  end
 end
