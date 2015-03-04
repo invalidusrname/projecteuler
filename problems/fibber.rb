8e05ea40a254c939ee762c1648a9adb079266de6
Matt McMahand <mmcmahand@gmail.com>
2015-03-03 20:04:07 -0500

    add pending specs for fibber
    - all the yaks have been shaved

---
+++ problems/fibber.rb
@@ -0,0 +1,2 @@
+class Fibber
+end
---
+++ spec/fibber_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Fibber do
+  it "generates the first 10 fibonacci numbers"
+  it "knows the sum of even numbers less than 100"
+end
